7d75518c665526fe2a0ce496b7ba774efe8e1b6a
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2025-01-12 10:47:04 +0600

    doctor house project start

---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,11 @@
+
+
+const Container = ({children}) => {
+    return (
+        <section className="max-w-screen-lg mx-auto">
+            {children}   
+        </section>
+    );
+};
+
+export default Container;
---
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import Container from "../../Container/Container";
+
+
+const Navbar = () => {
+    return (
+        <div className="bg-blue-300">
+            <Container>
+            <h1>this is navbar</h1>
+            </Container>
+        </div>
+    );
+};
+
+export default Navbar;
